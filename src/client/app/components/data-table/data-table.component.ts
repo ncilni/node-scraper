1789902589d92d1ae19043d690816d12fbd28ddc
Neeraj Chauhan <neeraj.chauhan@intelegencia.com>
2017-10-05 13:38:44 +0530

    Fix for the invalid css

--- src/client/app/components/data-table/data-table.component.ts
+++ src/client/app/components/data-table/data-table.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import 'C:/Users/neeraj.chauhan/Documents/July_2017/JumpCrew-Scraper/node_modules/@swimlane/ngx-datatable/release/index.css'
-import 'C:/Users/neeraj.chauhan/Documents/July_2017/JumpCrew-Scraper/node_modules/@swimlane/ngx-datatable/release/themes/material.css';
-import 'C:/Users/neeraj.chauhan/Documents/July_2017/JumpCrew-Scraper/node_modules/@swimlane/ngx-datatable/release/assets/icons.css';
+
 
 @Component({
   selector: 'app-data-table',
