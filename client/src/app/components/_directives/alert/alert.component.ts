e96062069b669df120a796d9c5075f94289375a3
rakesh.saini <rakesh.saini@intelegencia.com>
2017-11-09 14:56:57 +0530

    error handling at global level

--- client/src/app/components/_directives/alert/alert.component.ts
+++ client/src/app/components/_directives/alert/alert.component.ts
@@ -13,7 +13,18 @@ export class AlertComponent implements OnInit {
      constructor(private alertService: AlertService) { }
   
      ngOnInit() {
-         this.alertService.getMessage().subscribe(message => { this.message = message; });
+       
+         this.alertService.getMessage().subscribe(message => { 
+          console.log(message.text);
+          this.message = message; 
+          if(this.message.text.status == 500){
+            this.message.text.statusText += ". Please check network connection.";
+          }
+          else if(this.message.text.status == 401){
+            this.message.text.statusText += ". Not Authorized for this action."
+          }
+
+          });
      }
 
 }
