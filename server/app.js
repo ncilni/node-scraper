12cef154a9aaef510ecf0a3df60ef93b5f213b12
Neeraj Chauhan <neeraj.chauhan@intelegencia.com>
2017-11-09 15:30:15 +0530

    remove serve-favicon include statement

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var mlogger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
